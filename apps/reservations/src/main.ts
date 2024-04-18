ce7b8292d5965bcebe576c6d70159bc6d8f76c9a
Belendia Serda <belendia@gmail.com>
2024-04-18 09:50:47 +0300

    Add passport library for JWT and clean up the code

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
+
 import { ReservationsModule } from './reservations.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
 
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ReservationsService } from './reservations.service';
-import { ReservationsController } from './reservations.controller';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { DatabaseModule } from '@app/common';
+import { LoggerModule } from '@app/common';
+
 import { ReservationsRepository } from './reservations.repository';
 import {
   ReservationDocument,
   ReservationSchema,
 } from './models/reservation.schema';
-import { LoggerModule } from '@app/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
 
 @Module({
   imports: [
@@ -16,6 +19,13 @@ import { LoggerModule } from '@app/common';
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
